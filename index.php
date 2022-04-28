c0d9c4d798b9ea99eb809577c1893bf67b1661d3
bbblgmsp <71516034+bbblgmsp@users.noreply.github.com>
2022-04-28 21:14:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,15 +6,6 @@
 <div id="app">
 	<div>
 
-<!-- 
-		<form class="form" id="myForm">
- 		  <label>Upload file:</label>
-		  <input type="file" id="inpFile" />
-		  <br />
-		  <button type="submit">UPLOAD ALREADY</button>
-		</form>
--->
-
 		<form id="myForm" method="post" enctype="multipart/form-data" action="./">
 			<input id="inpFile" type="file" name="files[]" multiple>
 			<button type="submit" value="Upload File" name="submit">UPLOAD</button>
@@ -30,30 +21,6 @@
 
 <script type="text/javascript">
 			
-	// 	const myForm = document.getElementById("myForm");
-	// 	const inpFile = document.getElementById("inpFile");
-	// 	const result = document.getElementById("result");
-
-	// 	result.innerHTML = "OH the vast emptiness...";
-
-	// 	myForm.addEventListener("submit", e => {
-	// 		e.preventDefault();
-
-	// 		const endpoint = "/images/";
-	// 		const formData = new FormData();
-
-	// 		formData.append("inpFile", inpFile.files[0]);
-
-	// 		fetch(endpoint, {
-	// 			method: "post",
-	// 			body: formData,
-	// 		}).then ((response) => {
-	// 			console.log(response);
-	// 			if (response.status === 200) {result.innerHTML = "Dateien wurden geladen";}
-	// 		}).catch(console.error);
-	// 	});
-	// }
-
 	const url = "_upload.php";
 	const myForm = document.getElementById("myForm");
 
